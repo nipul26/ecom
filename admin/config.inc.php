7a0ead99ded08498b93fab7f5bbbfe723861f45f
Nipul Thoriya <nipulthoriya@Nipuls-MacBook-Air.local>
2024-06-23 19:23:18 +0530

    Added files for changes in header and session start

--- admin/config.inc.php
+++ admin/config.inc.php
@@ -1,8 +1,8 @@
 <?php 
-// session_start();
+session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL); 
-$conn = mysqli_connect("localhost","root","","ecom") or die("database connection failed");
+$conn = mysqli_connect("localhost","root","123456789","ecom") or die("database connection failed");
 date_default_timezone_set("Asia/Kolkata");
 ?>
