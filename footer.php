08c93c24e35766b3cfce262062ea0a32ffc1ee29
Meggi <meggi@192.168.22.109>
2024-07-03 22:44:35 +0530

    seprate the header and footer

---
+++ footer.php
@@ -0,0 +1,134 @@
+ <!-- ...::: Strat Footer Section - Footer Light :::... -->
+    <footer class="footer-section footer-section-style-1 section-top-gap-120">
+        <div class="box-wrapper">
+            <div class="footer-wrapper section-fluid-270">
+                <div class="container-fluid">
+
+                    <!-- Start Footer Top  -->
+                    <div class="footer-top">
+                        <div class="footer-top-left">
+                            <div class="footer-contact-items">
+                                <a class="icon-left" href="tel:+12345678910"><img class="icon-svg" src="assets/images/icons/icon-ios-call-dark.svg" alt="">123 4567 8910</a>
+                                <a class="icon-left" href="mailto:demo@example.com"><img class="icon-svg" src="assets/images/icons/icon-mail-open-dark.svg" alt="">demo@example.com</a>
+                            </div>
+                        </div>
+                        <div class="footer-top-right">
+                            <div class="footer-social">
+                                <a href="https://www.facebook.com/" target="_blank" rel="noopener"><img class="icon-svg" src="assets/images/icons/icon-facebook-f-dark.svg" alt=""></a>
+                                <a href="https://twitter.com/" target="_blank" rel="noopener"><img class="icon-svg" src="assets/images/icons/icon-twitter-dark.svg" alt=""></a>
+                                <a href="https://www.pinterest.com/" target="_blank" rel="noopener"><img class="icon-svg" src="assets/images/icons/icon-pinterest-p-dark.svg" alt=""></a>
+                                <a href="https://dribbble.com/" target="_blank" rel="noopener"><img class="icon-svg" src="assets/images/icons/icon-dribbble-dark.svg" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Footer Top  -->
+
+                    <!-- Start Footer Center  -->
+                    <div class="footer-center">
+                        <div class="footer-widgets-items">
+                            <!-- Start Footer Widget Single Item -->
+                            <div class="footer-widgets-single-item footer-widgets-single-item--light">
+                                <h5 class="title">Product</h5>
+                                <h5 class="collapsed-title collapsed" data-bs-toggle="collapse" data-bs-target="#dividerId-1">Product</h5>
+                                <div id="dividerId-1" class="widget-collapse-body collapse">
+                                    <ul class="footer-nav">
+                                        <li><a href="contact.html">Shop Vendor</a></li>
+                                        <li><a href="contact.html">Product House</a></li>
+                                        <li><a href="contact.html">Categories</a></li>
+                                        <li><a href="contact.html">Delivery Areas</a></li>
+                                        <li><a href="contact.html">Delivery Cost</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- End Footer Widget Single Item -->
+                            <!-- Start Footer Widget Single Item -->
+                            <div class="footer-widgets-single-item footer-widgets-single-item--light">
+                                <h5 class="title">Offer</h5>
+                                <h5 class="collapsed-title collapsed" data-bs-toggle="collapse" data-bs-target="#dividerId-2">Offer</h5>
+                                <div id="dividerId-2" class="widget-collapse-body collapse">
+                                    <ul class="footer-nav">
+                                        <li><a href="contact.html">Shop Vendor</a></li>
+                                        <li><a href="contact.html">Product House</a></li>
+                                        <li><a href="contact.html">Categories</a></li>
+                                        <li><a href="contact.html">Delivery Areas</a></li>
+                                        <li><a href="contact.html">Delivery Cost</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- End Footer Widget Single Item -->
+                            <!-- Start Footer Widget Single Item -->
+                            <div class="footer-widgets-single-item footer-widgets-single-item--light">
+                                <h5 class="title">Information</h5>
+                                <h5 class="collapsed-title collapsed" data-bs-toggle="collapse" data-bs-target="#dividerId-3">Information</h5>
+                                <div id="dividerId-3" class="widget-collapse-body collapse">
+                                    <ul class="footer-nav">
+                                        <li><a href="contact.html">Shop Vendor</a></li>
+                                        <li><a href="contact.html">Product House</a></li>
+                                        <li><a href="contact.html">Categories</a></li>
+                                        <li><a href="contact.html">Delivery Areas</a></li>
+                                        <li><a href="contact.html">Delivery Cost</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- End Footer Widget Single Item -->
+                            <!-- Start Footer Widget Single Item -->
+                            <div class="footer-widgets-single-item footer-widgets-single-item--light">
+                                <h5 class="title">About</h5>
+                                <h5 class="collapsed-title collapsed" data-bs-toggle="collapse" data-bs-target="#dividerId-4">About</h5>
+                                <div id="dividerId-4" class="widget-collapse-body collapse">
+                                    <ul class="footer-nav">
+                                        <li><a href="contact.html">Shop Vendor</a></li>
+                                        <li><a href="contact.html">Product House</a></li>
+                                        <li><a href="contact.html">Categories</a></li>
+                                        <li><a href="contact.html">Delivery Areas</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- End Footer Widget Single Item -->
+                        </div>
+                    </div>
+                    <!-- End Footer Center  -->
+
+                    <!-- Start Footer Bottom -->
+                    <div class="footer-bottom">
+                        <p class="copyright-text copyright-text--light">&copy; 2021 Mart Up. Made with <span class="material-icons">favorite</span> by <a href="https://hasthemes.com/" target="_blank" rel="noopener noreferrer">Codecarnival</a></p>
+                        <a href="#" class="payment-logo"><img class="img-fluid" src="assets/images/company-logo/payment-logo.png" alt=""></a>
+                    </div>
+                    <!-- End Footer Bottom -->
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ...::: End Footer Section Section - Footer Light :::... -->
+
+    <!-- Scroll To button -->
+    <div id="scroll-to-top" class="scroll-to-top"><span class="material-icons-outlined">expand_less</span></div>
+
+    <!-- ::::::::::::::All JS Files here :::::::::::::: -->
+    <!-- Global Vendor -->
+    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/vendor/jquery-ui.min.js"></script>
+
+    <!--Plugins JS-->
+    <script src="assets/js/plugins/jquery.nice-select.js"></script>
+    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="assets/js/plugins/ion.rangeSlider.min.js"></script>
+    <script src="assets/js/plugins/venobox.min.js"></script>
+    <script src="assets/js/plugins/ajax-mail.js"></script>
+
+    <!-- Minify Version -->
+    <!-- <script src="assets/js/vendor/vendor.min.js"></script> -->
+    <!-- <script src="assets/js/plugins/plugins.min.js"></script> -->
+
+    <!--Main JS (Common Activation Codes)-->
+    <script src="assets/js/main.js"></script>
+    <!-- <script src="assets/js/main.min.js"></script> -->
+
+</body>
+
+
+<!-- Mirrored from htmldemo.net/martup-v1/martup/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 17 Jun 2024 18:17:37 GMT -->
+</html>
